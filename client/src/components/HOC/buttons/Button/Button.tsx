1b293a3fd9a24166216521fe568d20d118bf71c8
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-04-12 10:44:43 +0300

    hardcoded it work in some way but too long time to modify it

--- client/src/components/HOC/buttons/Button/Button.tsx
+++ client/src/components/HOC/buttons/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 import style from "./Button.module.css";
 
 // const makeRandomMinMax = (min: number, max: number) =>
@@ -6,12 +6,34 @@ import style from "./Button.module.css";
 
 const Button: FC = () => {
   const btnRef = useRef<HTMLButtonElement | null>(null);
+  const bubbleRefs = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const makeBubbles = () => {};
+  }, []);
+
+  useEffect(() => {
+    const animate = (e: MouseEvent) => {
+      if (btnRef.current && bubbleRefs.current)
+        if (btnRef.current.contains(e.target as Node)) {
+          bubbleRefs.current.classList.remove(style.bubbles);
+          requestAnimationFrame(() => {
+            bubbleRefs.current?.classList.add(style.bubbles);
+          });
+        }
+    };
+
+    document.addEventListener("mousedown", animate);
+
+    return () => document.removeEventListener("mousedown", animate);
+  }, []);
 
   return (
     <button
       ref={btnRef}
       className={`appearance-none min-w-full border-2 border-blue-600 rounded-xl py-2 px-10 flex justify-center items-center ${style.btn__container}`}
     >
+      <div ref={bubbleRefs}></div>
       <span className="txt__3">Button</span>
     </button>
   );
