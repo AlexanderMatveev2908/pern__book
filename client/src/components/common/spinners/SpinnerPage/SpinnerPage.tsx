cae1ca0abbe2ef2aa01d367e65d63d778627362b
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-04-12 15:12:08 +0300

    made dummy spinner page

--- client/src/components/common/spinners/SpinnerPage/SpinnerPage.tsx
+++ client/src/components/common/spinners/SpinnerPage/SpinnerPage.tsx
@@ -1,20 +1,41 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import "./SpinnerPage.css";
 import { v4 } from "uuid";
+import { tailwindBreak } from "../../../../config/breakpoints";
 
 const SpinnerPage: FC = () => {
-  const [ids] = useState(Array.from({ length: 10 }, () => v4()));
+  const [ids, setIds] = useState(
+    Array.from({ length: window.innerWidth < tailwindBreak.sm ? 10 : 15 }, () =>
+      v4()
+    )
+  );
+
+  useEffect(() => {
+    const updateSize = () => {
+      setIds(
+        Array.from(
+          { length: window.innerWidth < tailwindBreak.sm ? 10 : 15 },
+          () => v4()
+        )
+      );
+    };
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
   return (
     <div className="el__spinner_page_container">
-      <div className="relative el__spinner_page_out">
+      <div className="relative">
         {ids.map((id, i) => (
           <div
             key={id}
             className="el__spinner_page_in"
-            style={{
-              rotate: `${(360 / ids.length) * i}deg`,
-            }}
+            style={
+              {
+                rotate: `${(360 / ids.length) * i}deg`,
+                "--delay_page": `${(i * 1) / ids.length}s`,
+              } as React.CSSProperties
+            }
           ></div>
         ))}
       </div>
