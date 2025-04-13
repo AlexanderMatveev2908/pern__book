4922b71399c9662836cdfb618b8486a25058c42e
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-04-13 22:33:23 +0300

    made wrapper window based

--- client/src/features/Header/components/DropDown.tsx
+++ client/src/features/Header/components/DropDown.tsx
@@ -2,10 +2,17 @@ import { FC, useEffect, useRef, useState } from "react";
 import { fieldsHeaderDropNonLogged } from "../../../config/fields/fields";
 import { Link } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
+import { tailwindBreak } from "../../../config/breakpoints";
 
 // USE_REF NINJAS 🥷🏼🥷🏼🥷🏼 VS RERENDER SUPERHERO 🦹🏼🦹🏼🦹🏼
 // USER ENTER THUMB IF CLICK IT OPEN AND CAN GO TO PAGE HE WANT ON CLICK OF LINK,
-// IF HE DOES NOTHING BUT LEAVE I LET DROP OPEN A LITTLE
+// IF HE DOES NOTHING BUT LEAVE I LET DROP OPEN 250 MS SO IF DROP IS A LITTLE TOO FAR FROM THUMB U HAVE TIME TO GO THERE AND WHEN LEAVING DROP CONTENT I JUST SET DROP TO FALSE WITHOUT TIMER
+
+const windowWrapper = (cb: () => void) => {
+  if (window.innerWidth < tailwindBreak.md) return;
+
+  return cb();
+};
 
 const DropDown: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -51,13 +58,17 @@ const DropDown: FC = () => {
         }}
         className="w-fit p-1"
         onMouseEnter={() => {
-          setHasClicked(false);
-          setIsOpen(true);
+          windowWrapper(() => {
+            setHasClicked(false);
+            setIsOpen(true);
+          });
         }}
         onMouseLeave={() => {
           if (hasClicked) return;
-          setIsOpen(false);
-          setIsLeaving(true);
+          windowWrapper(() => {
+            setIsOpen(false);
+            setIsLeaving(true);
+          });
         }}
       >
         <FaRegUser className="icon__md icon__logic" />
@@ -66,14 +77,16 @@ const DropDown: FC = () => {
       <div
         ref={dropRef}
         onMouseEnter={() => {
-          clearTimeout(timerRef.current as NodeJS.Timeout);
-          timerRef.current = null;
+          windowWrapper(() => {
+            clearTimeout(timerRef.current as NodeJS.Timeout);
+            timerRef.current = null;
 
-          setIsLeaving(false);
-          setIsOpen(true);
+            setIsLeaving(false);
+            setIsOpen(true);
+          });
         }}
         onMouseLeave={() => {
-          setIsOpen(false);
+          windowWrapper(() => setIsOpen(false));
         }}
         className={`absolute top-full -left-[500%] bg-[#000] el__border_sm p-3 grid gap-3 min-w-[250px] transition-all duration-500   ${
           isLeaving || isOpen
