06370aaac6dd4f10596609ddcf27f5c9a961fec1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-04-29 13:34:05 +0300

    added remove worker btn

--- client/src/components/forms/BookStore/components/TeamForm/TeamForm.tsx
+++ client/src/components/forms/BookStore/components/TeamForm/TeamForm.tsx
@@ -6,7 +6,15 @@ import {
 } from "@/config/fields/OwnerLayout/post";
 import { FormField } from "@/components/components";
 import MySelect from "@/components/forms/components/inputs/MySelect";
-import { UserRole } from "@/types/types";
+import { BtnAct, UserRole } from "@/types/types";
+import { FaTrashAlt } from "react-icons/fa";
+import { AiOutlineUserDelete } from "react-icons/ai";
+import ButtonIcon from "@/components/common/buttons/ButtonIcon/ButtonIcon";
+
+const btnRemoveWorker = {
+  label: "Remove",
+  icon: AiOutlineUserDelete,
+};
 
 const TeamForm: FC = () => {
   const {
@@ -48,23 +56,37 @@ const TeamForm: FC = () => {
   console.log(errors);
 
   return (
-    <div className="w-full grid grid-cols-1 gap-5">
+    <div className="w-full grid ">
       {fields.map((el, i) => (
-        <div key={el.id} className="book_store_sub_form">
-          <FormField
-            {...{ register, errors, index: i, el: fieldEmailWorker }}
-          />
+        <div
+          key={el.id}
+          className="w-full grid grid-cols-1 sm:grid-cols-2 gap-y-3 gap-x-10 p-4 border-[3px] border-blue-600 rounded-xl"
+        >
+          <div className="book_store_sub_form">
+            <FormField
+              {...{ register, errors, index: i, el: fieldEmailWorker }}
+            />
 
-          <MySelect
-            {...{
-              index: i,
-              el: fieldSelectWorkerRole,
-              errors,
-              handleClick: handleClickSelect,
-              checkIsIn,
-              currVal: getCurrVal(i),
-            }}
-          />
+            <MySelect
+              {...{
+                index: i,
+                el: fieldSelectWorkerRole,
+                errors,
+                handleClick: handleClickSelect,
+                checkIsIn,
+                currVal: getCurrVal(i),
+              }}
+            />
+          </div>
+          <div className="w-[250px] justify-self-center">
+            <ButtonIcon
+              {...{
+                handleClick: () => console.log("todo"),
+                el: btnRemoveWorker,
+                act: BtnAct.DEL,
+              }}
+            />
+          </div>
         </div>
       ))}
     </div>
