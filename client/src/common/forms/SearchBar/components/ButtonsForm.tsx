ce7811306c4ea8c4311671a9a3f0a45898e43af0
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-06 12:39:34 +0300

    start arr fields

--- client/src/common/forms/SearchBar/components/ButtonsForm.tsx
+++ client/src/common/forms/SearchBar/components/ButtonsForm.tsx
@@ -23,7 +23,7 @@ const ButtonsForm: FC<PropsType> = ({ txtInputs, setFocus }) => {
 
   return (
     <div className="w-full grid grid-cols-1 h-fit items-start gap-y-5 gap-x-10 search_bar__btns">
-      <div className="w-full grid grid-cols-2 gap-x-10 items-center search_bar_btns_right">
+      <div className="w-full grid grid-cols-2 lg:grid-cols-[1fr_75px] gap-x-10 items-center search_bar_btns_right">
         <div className={`w-full justify-self-center ${getSize(labelSubmit)}`}>
           <ButtonIcon
             {...{
