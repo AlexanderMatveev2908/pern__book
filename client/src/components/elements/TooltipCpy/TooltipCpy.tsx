b70e346d85d871b788329154874962a06bccddcb
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-09 16:05:23 +0300

    change tooltip animation

--- client/src/components/elements/TooltipCpy/TooltipCpy.tsx
+++ client/src/components/elements/TooltipCpy/TooltipCpy.tsx
@@ -69,7 +69,7 @@ const TooltipCpy: FC<PropsType> = ({ txt, bd = true }) => {
         className="absolute el__border_sm -top-[120%] py-1 px-5 z-60 bg-[#000] left-0 min-w-[200px] flex justify-center tooltip pointer-events-none"
         style={{
           opacity: "0",
-          transform: "translateY(50px)",
+          transform: "scale(0)",
         }}
       >
         <span className="txt__1">Copied to clipboard</span>
