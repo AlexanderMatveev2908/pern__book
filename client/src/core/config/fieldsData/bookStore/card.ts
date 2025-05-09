8ac4f19316318b4e097c8d3b374cc2187f626056
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-09 22:05:32 +0300

    added links

--- client/src/core/config/fieldsData/bookStore/card.ts
+++ client/src/core/config/fieldsData/bookStore/card.ts
@@ -1,5 +1,8 @@
+import { FaLink } from "react-icons/fa";
+import { GrUpdate } from "react-icons/gr";
 import { HiLibrary } from "react-icons/hi";
 import { IoIosStats } from "react-icons/io";
+import { v4 } from "uuid";
 
 export const labelCardStore = (name: string) => ({
   label: name,
@@ -10,3 +13,19 @@ export const statsCardStore = {
   label: "Info",
   icon: IoIosStats,
 };
+
+export const linksCardStore = [
+  {
+    icon: FaLink,
+    label: "Page Store",
+    path: "/owner/book-store/",
+  },
+  {
+    icon: GrUpdate,
+    label: "Update",
+    path: "/owner/book-store/update/",
+  },
+].map((el) => ({
+  ...el,
+  id: v4(),
+}));
--- client/src/pages/OwnerLayout/BookStoreLayout/BookStores/components/BookStoreItem.tsx
+++ client/src/pages/OwnerLayout/BookStoreLayout/BookStores/components/BookStoreItem.tsx
@@ -2,8 +2,10 @@ import InfoCardStats from "@/components/elements/cards/bookstore/itemList/InfoCa
 import InfoCardStore from "@/components/elements/cards/bookstore/itemList/InfoCardStore/InfoCardStore";
 import ImagesItem from "@/components/elements/cards/shared/ImagesItem";
 import ItemID from "@/components/elements/cards/shared/ItemID";
+import { linksCardStore } from "@/core/config/fieldsData/bookStore/card";
 import { BookStoreType } from "@/types/all/bookStore";
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 type PropsType = {
   el: BookStoreType;
@@ -11,7 +13,7 @@ type PropsType = {
 
 const BookStoreItem: FC<PropsType> = ({ el }) => {
   return (
-    <div className="w-full grid grid-cols-1 h-fit items-start border-[3px] gap-y-5 border-blue-600 p-3 rounded-xl  justify-self-center">
+    <div className="w-full grid grid-cols-1 h-fit items-start border-[3px] gap-y-5 border-blue-600 p-3 pb-5 rounded-xl  justify-self-center">
       <ItemID {...{ ID: el.id }} />
       <div className="w-full grid grid-cols-1 gap-x-5 gap-y-5">
         <div className="w-full grid grid-cols-1 h-fit items-start">
@@ -22,6 +24,20 @@ const BookStoreItem: FC<PropsType> = ({ el }) => {
           <InfoCardStore {...{ el }} />
           <InfoCardStats {...{ el }} />
         </div>
+      </div>
+
+      <div className="w-full grid grid-cols-2 justify-items-center -mt-3">
+        {linksCardStore.map((lin) => (
+          <Link
+            key={lin.id}
+            to={lin.path + el.id}
+            className="px-4 py-1 flex justify-center gap-5 items-center el__flow hover:text-blue-600 el__after_below"
+          >
+            <lin.icon className="icon__md" />
+
+            <span className="txt__2">{lin.label}</span>
+          </Link>
+        ))}
       </div>
     </div>
   );
