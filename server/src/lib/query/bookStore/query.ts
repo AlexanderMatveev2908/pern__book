90fde6ef6123d4eb2fdf810dbc8925b2d6f2d155
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-10 22:37:43 +0300

    fix [Op] of sequlize, in was wrong as opt to check overlap of two array

--- server/src/lib/query/bookStore/query.ts
+++ server/src/lib/query/bookStore/query.ts
@@ -18,21 +18,32 @@ export const createStoreQ = (req: ReqApp) => {
   } = req.query;
   const { userID } = req;
 
-  const query: any = {
+  const query: { [key: string]: unknown } = {
     ownerID: userID,
   };
 
-  if (Object.keys(req.query ?? {}).length) {
-    for (const key in req.query) {
-      const val = req.query[key];
-      if (!val || (Array.isArray(val) && !val.length)) continue;
+  if (!Object.keys(req.query ?? {}).length) return query;
 
-      if (["name", "country", "state", "city"].includes(key))
-        query[key] = {
-          [Op.iLike]: `%${val}%`,
-        };
+  for (const key in req.query) {
+    const val = req.query[key];
+    if (
+      (typeof val == "string" && !val.trim().length) ||
+      (Array.isArray(val) && !val.length)
+    )
+      continue;
 
-      if (key === "ID") query.id = val;
+    if (["name", "country", "state", "city"].includes(key))
+      query[key] = {
+        [Op.iLike]: `%${val}%`,
+      };
+
+    if (key === "ID") query.id = val;
+
+    if (key === "categories") {
+      query.categories = {
+        [Op.overlap]: Array.isArray(val) ? val : [val],
+        // [Op.contains]: Array.isArray(val) ? val : [val],
+      };
     }
   }
 
