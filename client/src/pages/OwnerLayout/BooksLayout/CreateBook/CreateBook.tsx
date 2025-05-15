e31bb233c1335d5eb0031042c32442814d3e9500
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-15 17:55:23 +0300

    fix outlet push

--- client/src/pages/OwnerLayout/BooksLayout/CreateBook/CreateBook.tsx
+++ client/src/pages/OwnerLayout/BooksLayout/CreateBook/CreateBook.tsx
@@ -1,22 +1,27 @@
 import BookForm from "@/common/forms/BookForm/BookForm";
 import Title from "@/components/elements/Title";
+import WrapPageAPI from "@/components/HOC/WrapPageAPI";
 import { useFormCtxConsumer } from "@/core/contexts/FormsCtx/hooks/useFormCtxConsumer";
+import { useGetU } from "@/core/hooks/all/useGetU";
 import type { FC } from "react";
 import { FormProvider } from "react-hook-form";
 
 const CreateBook: FC = () => {
   const { createBookFormCtx: formCtx } = useFormCtxConsumer();
+  const { user, isLoading, error, isError } = useGetU();
 
   return (
-    <div className="parent__page">
-      <Title {...{ title: "add book" }} />
+    <WrapPageAPI {...{ canStay: user?.isOwner, isError, error, isLoading }}>
+      <div className="parent__page">
+        <Title {...{ title: "add book" }} />
 
-      <div className="w-full grid justify-items-center gap-6">
-        <FormProvider {...formCtx}>
-          <BookForm />
-        </FormProvider>
+        <div className="w-full grid justify-items-center gap-6">
+          <FormProvider {...formCtx}>
+            <BookForm />
+          </FormProvider>
+        </div>
       </div>
-    </div>
+    </WrapPageAPI>
   );
 };
 
