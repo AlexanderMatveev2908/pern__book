e75f833cd074ff5baabe80d3ceef2e67f5ca2ad8
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-25 10:51:06 +0300

    _

--- client/tests/Home.test.tsx
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("homepage ", async ({ page }) => {
-  await page.goto("http://localhost:3001");
-  await expect(page.getByText("Work in progress ⚒️⚒️⚒️")).toBeVisible();
-});
