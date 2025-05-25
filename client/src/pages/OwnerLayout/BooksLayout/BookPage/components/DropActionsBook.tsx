9ebf17cbde342e1d2e0f5b6b19629dc9166220ed
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-25 15:40:40 +0300

    made delete book reusable_custom_hook

--- client/src/pages/OwnerLayout/BooksLayout/BookPage/components/DropActionsBook.tsx
+++ client/src/pages/OwnerLayout/BooksLayout/BookPage/components/DropActionsBook.tsx
@@ -1,17 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import DropActionsObj from "@/components/elements/cards/shared/DropActionsHOC/DropActionsObj";
 import { manageDropLabelGeneral } from "@/core/config/fieldsData/general/labels";
 import { actionsBookPage } from "@/core/config/fieldsData/OwnerLayout/books/actions";
-import { useWrapMutationAPI } from "@/core/hooks/hooks";
-import {
-  closePopup,
-  loadPop,
-  openPopup,
-} from "@/features/common/Popup/popupSlice";
+import { useDeleteBook } from "@/core/hooks/all/forms/books/useDeleteBook";
 import { booksSLiceAPI } from "@/features/OwnerLayout/books/booksSliceAPI";
 import { BookType } from "@/types/all/books";
-import { BtnAct, BtnPopupKeys } from "@/types/types";
-import { useCallback, useMemo, type FC } from "react";
-import { useDispatch } from "react-redux";
+import { useMemo, type FC } from "react";
 import { useNavigate } from "react-router-dom";
 
 type PropsType = {
@@ -21,41 +15,13 @@ type PropsType = {
 const DropActionsBook: FC<PropsType> = ({ book }) => {
   const nav = useNavigate();
 
-  const dispatch = useDispatch();
   const [mutateDelete] = booksSLiceAPI.useDeleteBookMutation();
-  const { wrapMutationAPI } = useWrapMutationAPI();
 
-  const handleOpenPopup = useCallback(
-    () =>
-      dispatch(
-        openPopup({
-          txt: "Are You sure about deleting this book ?",
-          leftBtn: {
-            label: "Delete",
-            act: BtnAct.DEL,
-            cb: async () => {
-              dispatch(loadPop(BtnPopupKeys.LEFT));
-
-              const res = await wrapMutationAPI({
-                cbAPI: () => mutateDelete(book.id as string),
-              });
-
-              dispatch(closePopup());
-
-              if (!res) return;
-
-              nav(`/owner/book-store/${book.bookStoreID}`, { replace: true });
-            },
-          },
-          rightBtn: {
-            label: "Cancel",
-            act: BtnAct.DO,
-            cb: () => dispatch(closePopup()),
-          },
-        })
-      ),
-    [book, dispatch, mutateDelete, nav, wrapMutationAPI]
-  );
+  const { handleOpenPopup } = useDeleteBook({
+    mutateDelete,
+    _path: `/owner/book-store/${book?.store?.id}`,
+    ID: book.id,
+  } as any);
 
   const handlers = useMemo(
     () => ({
