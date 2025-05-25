2163a64b9a3a26fc180e649c7e3f4c28f3b500d0
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-25 16:10:21 +0300

    test route delete book, not_allowed user is pushed to notice page with 403

--- server/src/controllers/workerRouter/books/delete.ts
+++ server/src/controllers/workerRouter/books/delete.ts
@@ -1,10 +1,53 @@
 import { Response } from "express";
-import { ReqApp } from "../../../types/types.js";
+import { ReqApp, UserRole } from "../../../types/types.js";
 import { res200 } from "../../../lib/responseClient/res.js";
+import { Book } from "../../../models/all/Book.js";
+import { BookStore } from "../../../models/all/BookStore.js";
+import { User } from "../../../models/models.js";
+import { err403 } from "../../../lib/responseClient/err.js";
+import { delArrCloud } from "../../../lib/cloud/delete.js";
 
 export const deleteBookWorker = async (req: ReqApp, res: Response) => {
   const { userID } = req;
   const { bookID } = req.params;
+
+  const book = await Book.findOne({
+    where: {
+      id: bookID,
+    },
+    include: [
+      {
+        model: BookStore,
+        as: "store",
+        required: true,
+        include: [
+          {
+            model: User,
+            as: "team",
+            required: true,
+            through: {
+              as: "bookStoreUsers",
+              where: {
+                userID,
+                role: UserRole.MANAGER,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  if (!book) return err403(res, { msg: "book not found or user not allowed" });
+
+  await Book.destroy({
+    where: {
+      id: bookID,
+    },
+  });
+
+  if (book.images?.length)
+    await delArrCloud(book.images.map((img) => img.publicID));
 
   return res200(res, { msg: "book deleted" });
 };
