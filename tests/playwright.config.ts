6c0170a5e5aa194304079621713b6703c19e1faa
alex <matveevalexander470@gmail.com>
2025-05-25 13:39:46 +0300

    fix switch book

---
+++ tests/playwright.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./e2e", // Only look in E2E directory
+  testMatch: "**/*.test.ts?(x)", // Match Playwright test files only
+  // Other config like timeout, reporter, etc.
+});
