b1029eb8621f18de1fc6f0c5ad29563864f04e59
alex <matveevalexander470@gmail.com>
2025-05-25 15:40:40 +0300

    made delete book reusable_custom_hook

--- client/src/pages/WorkerLayout/BooksLayout/BookPageWorker/components/DropActionsBookWorker.tsx
+++ client/src/pages/WorkerLayout/BooksLayout/BookPageWorker/components/DropActionsBookWorker.tsx
@@ -2,6 +2,8 @@
 import DropActionsObj from "@/components/elements/cards/shared/DropActionsHOC/DropActionsObj";
 import { manageDropLabelGeneral } from "@/core/config/fieldsData/general/labels";
 import { actionsBookPageWorker } from "@/core/config/fieldsData/WorkerLayout/books/actions";
+import { useDeleteBook } from "@/core/hooks/all/forms/books/useDeleteBook";
+import { booksSliceWorkerAPI } from "@/features/WorkerLayout/Books/booksSliceWorkerAPI";
 import { BookType } from "@/types/all/books";
 import { UserRole } from "@/types/types";
 import { useMemo, type FC } from "react";
@@ -34,14 +36,24 @@ const DropActionsBookWorker: FC<PropsType> = ({ book }) => {
     [book]
   );
 
+  const [mutateDelete] = booksSliceWorkerAPI.useDeleteBookWorkerMutation();
+  const { handleOpenPopup } = useDeleteBook({
+    mutateDelete,
+    _path: `/worker/book-stores/${book?.store?.id}`,
+    ID: book?.id,
+  } as any);
+
   const handlers = Object.fromEntries(
     Object.entries(filteredActions).map(([k, v]) => {
       switch (k) {
         case "store":
           return [k, () => nav(`${v.path}${book?.store?.id}`)];
 
+        case "delete":
+          return [k, handleOpenPopup];
+
         default:
-          return [[k], () => nav(`${v.path}${book?.id}`)];
+          return [k, () => nav(`${v.path}${book?.id}`)];
       }
     })
   );
