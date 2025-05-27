9cdfaf65e398c4deb4aaa8608a3ec23c5e8a33e0
alex <matveevalexander470@gmail.com>
2025-05-27 10:36:48 +0300

    deleted declare keyword cause would require to write a babel condifg fromscratch

---
+++ tests/src/db/types/all/orders.ts
@@ -0,0 +1,10 @@
+export enum OrderStage {
+  PENDING = "pending",
+  PAID = "paid",
+  PROCESSING = "processing",
+  SHIPPED = "shipped",
+  DELIVERED = "delivered",
+  COMPLETED = "completed",
+  CANCELLED = "cancelled",
+  REFUNDED = "refunded",
+}
--- tests/src/e2e/Home.test.tsx
+++ tests/src/e2e/Home.test.tsx
@@ -1,9 +1,18 @@
 import { test, expect } from "@playwright/test";
+import seq from "../db/config.js";
+import { bindModels } from "../db/models/all/bind/bind.js";
+import { User } from "../db/models/all/User.js";
 
-test.beforeAll(async () => {});
+test.beforeAll(async () => {
+  await seq.authenticate();
+  bindModels(seq);
+});
 
 test("goes to localhost:3001 home page", async ({ page }) => {
   await page.goto("http://localhost:3001");
+
+  const users = await User.findAll({ where: {} });
+  console.log(users);
 
   await expect(page.locator("text=Work in progress ⚒️⚒️⚒️")).toBeVisible();
 });
