cd361dd6bdad7d543f35855b18592e0fd34651c8
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-27 14:09:08 +0300

    _

--- tests/src/e2e/auth/auth.test.tsx
+++ tests/src/e2e/auth/auth.test.tsx
@@ -34,7 +34,7 @@ test.beforeAll(async () => {
   await clear();
 });
 test.afterAll(async () => {
-  await clear();
+  // await clear();
 });
 
 // test.describe("handle non existent user", () => {
@@ -67,61 +67,63 @@ test.afterAll(async () => {
 //   });
 // });
 
-test.describe("handle new user", () => {
-  test("should register", async ({ page }) => {
-    await nav(page, "auth/register");
+// test.describe("handle new user", () => {
+//   test("should register", async ({ page }) => {
+//     await nav(page, "auth/register");
 
-    await page
-      .getByRole("textbox", { name: "First Name" })
-      .fill(account_0.firstName);
-    await page
-      .getByRole("textbox", { name: "Last Name" })
-      .fill(account_0.lastName);
-    await page.getByRole("textbox", { name: "email" }).fill(account_0.email);
+//     await page
+//       .getByRole("textbox", { name: "First Name" })
+//       .fill(account_0.firstName);
+//     await page
+//       .getByRole("textbox", { name: "Last Name" })
+//       .fill(account_0.lastName);
+//     await page.getByRole("textbox", { name: "email" }).fill(account_0.email);
 
-    await page.getByRole("button", { name: "next" }).click();
+//     await page.getByRole("button", { name: "next" }).click();
 
-    await page.waitForSelector("text=Password", { state: "visible" });
-    await expect(page.getByText("Password", { exact: true })).toBeVisible();
-    await page.waitForSelector("text=Generate password", { state: "visible" });
-    await expect(page.getByText("Generate password")).toBeVisible();
+//     await page.waitForSelector("text=Password", { state: "visible" });
+//     await expect(page.getByText("Password", { exact: true })).toBeVisible();
+//     await page.waitForSelector("text=Generate password", { state: "visible" });
+//     await expect(page.getByText("Generate password")).toBeVisible();
 
-    await page.getByRole("button", { name: "Generate password" }).click();
-    await page
-      .locator(".tooltip_cpy > div > .txt__2")
-      .waitFor({ state: "visible" });
-    await expect(page.locator(".tooltip_cpy > div > .txt__2")).toHaveText(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-zÀ-ÿ\d\W_]{16}$/
-    );
+//     // await page.getByRole("button", { name: "Generate password" }).click();
+//     // await page
+//     //   .locator(".tooltip_cpy > div > .txt__2")
+//     //   .waitFor({ state: "visible" });
+//     // await expect(page.locator(".tooltip_cpy > div > .txt__2")).toHaveText(
+//     //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-zÀ-ÿ\d\W_]{16}$/
+//     // );
 
-    await page.getByRole("button", { name: "copy to clipboard" }).click();
-    await expect(page.getByText("Copied to clipboard")).toBeVisible();
-    const pwd = await page
-      .locator(".tooltip_cpy > div > .txt__2") // or whatever element holds just the password
-      .textContent();
+//     // await page.getByRole("button", { name: "copy to clipboard" }).click();
+//     // await expect(page.getByText("Copied to clipboard")).toBeVisible();
+//     // const pwd = await page
+//     //   .locator(".tooltip_cpy > div > .txt__2") // or whatever element holds just the password
+//     //   .textContent();
 
-    await page.getByLabel("Password", { exact: true }).fill(pwd ?? "");
-    await page
-      .getByRole("textbox", { name: "Confirm Password" })
-      .fill(pwd ?? "");
+//     await page
+//       .getByLabel("Password", { exact: true })
+//       .fill(account_0.pwd ?? "");
+//     await page
+//       .getByRole("textbox", { name: "Confirm Password" })
+//       .fill(account_0.pwd ?? "");
 
-    await page
-      .getByRole("checkbox", { name: "accept terms and conditions" })
-      .click();
-    await expect(
-      page.getByRole("checkbox", { name: "accept terms and conditions" })
-    ).toHaveClass(/checky/);
-    await expect(
-      page.getByRole("checkbox", { name: "accept terms and conditions" })
-    ).toHaveAttribute("aria-checked", "true");
+//     await page
+//       .getByRole("checkbox", { name: "accept terms and conditions" })
+//       .click();
+//     await expect(
+//       page.getByRole("checkbox", { name: "accept terms and conditions" })
+//     ).toHaveClass(/checky/);
+//     await expect(
+//       page.getByRole("checkbox", { name: "accept terms and conditions" })
+//     ).toHaveAttribute("aria-checked", "true");
 
-    await page.getByRole("button", { name: "Register" }).click();
-    await expect(page.getByRole("button", { name: "Register" })).toBeHidden();
+//     await page.getByRole("button", { name: "Register" }).click();
+//     await expect(page.getByRole("button", { name: "Register" })).toBeHidden();
 
-    await page.waitForURL("**/notice");
-    await expect(page.getByText("Account created".toUpperCase())).toBeVisible();
-    await expect(
-      page.getByText(makeNoticeTxt("to verify your account"))
-    ).toBeVisible();
-  });
-});
+//     await page.waitForURL("**/notice");
+//     await expect(page.getByText("Account created".toUpperCase())).toBeVisible();
+//     await expect(
+//       page.getByText(makeNoticeTxt("to verify your account"))
+//     ).toBeVisible();
+//   });
+// });
