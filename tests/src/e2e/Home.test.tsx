2e3ae45163e8cb7f78f8ec3f8ca427a3f6a5a7fd
alex <matveevalexander470@gmail.com>
2025-05-27 10:03:27 +0300

    _

--- tests/src/e2e/Home.test.tsx
+++ tests/src/e2e/Home.test.tsx
@@ -1,10 +1,9 @@
 import { test, expect } from "@playwright/test";
-import seq from "./seq";
+
+test.beforeAll(async () => {});
 
 test("goes to localhost:3001 home page", async ({ page }) => {
   await page.goto("http://localhost:3001");
-
-  console.log(seq.models);
 
   await expect(page.locator("text=Work in progress ⚒️⚒️⚒️")).toBeVisible();
 });
