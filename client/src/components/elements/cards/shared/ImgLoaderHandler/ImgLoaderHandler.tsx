920ba89913e7c573ac2f75bba5708fc80606d7a2
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-29 14:22:27 +0300

    added rating in client card

--- client/src/components/elements/cards/shared/ImgLoaderHandler/ImgLoaderHandler.tsx
+++ client/src/components/elements/cards/shared/ImgLoaderHandler/ImgLoaderHandler.tsx
@@ -1,12 +1,15 @@
 import { useState, type FC } from "react";
+import { MdError } from "react-icons/md";
 
 type PropsType = {
   url: string;
   customClass?: string;
+  children?: React.ReactNode;
 };
 
-const ImgLoaderHandler: FC<PropsType> = ({ url, customClass }) => {
+const ImgLoaderHandler: FC<PropsType> = ({ url, customClass, children }) => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   return !isLoaded ? (
     <div
@@ -17,10 +20,23 @@ const ImgLoaderHandler: FC<PropsType> = ({ url, customClass }) => {
         alt="🥷🏼"
         className="opacity-0 absolute max-w-0 max-h-0"
         onLoad={() => setIsLoaded(true)}
+        onError={(err) => {
+          setIsError(true);
+          setIsLoaded(true);
+          console.log(err);
+        }}
       />
     </div>
+  ) : isError ? (
+    <div className="w-full h-full grid grid-cols-1 place-content-center place-items-center">
+      <span className="txt__6 text-red-600">404</span>
+      <MdError className="text-red-600 w-1/2 h-1/2" />
+    </div>
   ) : (
-    <img src={url} alt="✌🏼" className="object-contain w-full h-full" />
+    <>
+      <img src={url} alt="✌🏼" className="object-contain w-full h-full" />
+      {children}
+    </>
   );
 };
 
