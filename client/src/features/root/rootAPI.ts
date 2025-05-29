b3dd78a33590f200a17e46a92459cd79bb2e75d3
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-29 08:37:22 +0300

    building logic books all users

--- client/src/features/root/rootAPI.ts
+++ client/src/features/root/rootAPI.ts
@@ -1,4 +1,6 @@
 import apiSlice from "@/store/apiSlice";
+import { BookType } from "@/types/all/books";
+import { BaseResAPI, TagsAPI } from "@/types/types";
 
 export const rootAPI = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -7,6 +9,27 @@ export const rootAPI = apiSlice.injectEndpoints({
         url: "/user/security",
         method: "DELETE",
       }),
+    }),
+    getBooksByBestRating: builder.query<
+      BaseResAPI<{ books: BookType[] }>,
+      void
+    >({
+      query: () => ({
+        url: "/search/best-avg-rating",
+        method: "GET",
+      }),
+      providesTags: (res) => [
+        ...(res?.books?.length
+          ? res.books.map((el) => ({
+              type: TagsAPI.BOOKS_BEST_RATING,
+              id: el.id,
+            }))
+          : []),
+        {
+          type: TagsAPI.BOOKS_BEST_RATING,
+          id: "LIST",
+        },
+      ],
     }),
   }),
 });
