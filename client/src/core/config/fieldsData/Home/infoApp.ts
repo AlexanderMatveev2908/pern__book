9721751251917a585148203b6ba3e48e4abee69d
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-05-30 15:43:22 +0300

    added last card

--- client/src/core/config/fieldsData/Home/infoApp.ts
+++ client/src/core/config/fieldsData/Home/infoApp.ts
@@ -1,4 +1,6 @@
-import { BookShelf, FastDeploy } from "@/components/svgs";
+import SvgBookShelf from "@/components/svgs/BookShelf";
+import SvgFastDeploy from "@/components/svgs/FastDeploy";
+import SvgOpenSource from "@/components/svgs/OpenSource";
 import { v4 } from "uuid";
 
 type ItemHomeArgs = {
@@ -9,12 +11,16 @@ type ItemHomeArgs = {
 
 export const infosAppHome: ItemHomeArgs[] = [
   {
-    CompSVG: FastDeploy,
+    CompSVG: SvgFastDeploy,
     txt: `Launch your store online with a few simple, seamless steps — and start managing your business effortlessly`,
   },
   {
-    CompSVG: BookShelf,
+    CompSVG: SvgBookShelf,
     txt: `Keep track of your stock items and collaborate with your employees using a role-based system to manage access and permissions`,
+  },
+  {
+    CompSVG: SvgOpenSource,
+    txt: `Got any ideas? We're always open to feature requests and suggestions. The code is available on GitHub!`,
   },
 ].map((el) => ({
   ...el,
