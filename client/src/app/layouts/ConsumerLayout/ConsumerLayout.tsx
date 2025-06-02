f8051b8ab29ae17fba8b1f7b965e630813a496ac
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-02 19:44:58 +0300

    refacotr routes

--- client/src/app/layouts/ConsumerLayout/ConsumerLayout.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react";
-import { Outlet } from "react-router-dom";
-
-const ConsumerLayout: FC = () => {
-  return <Outlet />;
-};
-
-export default ConsumerLayout;
--- client/src/app/layouts/OwnerLayout/BookStoreLayout.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { Outlet } from "react-router-dom";
-
-const BookStoreLayout: FC = () => {
-  return <Outlet />;
-};
-
-export default BookStoreLayout;
