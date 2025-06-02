a05d23fd7d7803ac2a5e851ffd28fc3103061055
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-02 18:34:22 +0300

    _

--- client/src/pages/ConsumerLayout/books/BooksListConsumer/components/BookItemConsumer.tsx
+++ client/src/pages/ConsumerLayout/books/BooksListConsumer/components/BookItemConsumer.tsx
@@ -1,15 +1,38 @@
-import ConsumerFieldsBook from "@/features/ConsumerLayout/books/components/ConsumerFieldsBook";
 import ImagesItem from "@/components/elements/imagesHandlers/ImagesItem";
 import LinksCard from "@/components/elements/cards/shared/LinksCard";
-import { linksBookConsumer } from "@/features/ConsumerLayout/books/fields/card";
+import {
+  linksBookConsumer,
+  showStoreAddressFromBook,
+  statsDeliveryStoreFromBook,
+} from "@/features/ConsumerLayout/books/fields/card";
 import { BookType } from "@/types/all/books";
 import type { FC } from "react";
+import DropStats from "@/components/elements/dropMenus/dropSimple/DropStats";
+import {
+  labelBookCard,
+  labelDataBook,
+  showGeneralStatsBook,
+} from "@/core/config/fieldsData/books/cards";
+import InfoBookAbout from "@/components/elements/cards/books/subComponents/InfoBookAbout";
+import {
+  labelCategories,
+  labelInfo,
+  libraryLabelStoreDynamic,
+} from "@/core/config/fieldsData/labels/shared";
+import RatingFancy from "@/components/elements/cards/shared/rating/RatingFancy";
+import {
+  labelDelivery,
+  labelFieldAddressStore,
+} from "@/core/config/fieldsData/bookStores/cards";
+import { useCreateIds } from "@/core/hooks/all/UI/useCreateIds";
 
 type PropsType = {
   el: BookType;
 };
 
 const BookItemConsumer: FC<PropsType> = ({ el }) => {
+  const ids = useCreateIds({ lengths: [el?.store?.categories?.length] });
+
   return (
     <div className="c_card">
       <div className="w-full grid grid-cols-1 gap-x-5 gap-y-5">
@@ -18,11 +41,64 @@ const BookItemConsumer: FC<PropsType> = ({ el }) => {
         </div>
 
         <div
-          className={`w-full grid grid-cols-1 gap-3 sm:h-fit sm:items-start ${
+          className={`w-full grid grid-cols-1 gap-4 sm:h-fit sm:items-start ${
             el.images?.length ? "" : "-mt-5"
           }`}
         >
-          <ConsumerFieldsBook {...{ el }} />
+          <DropStats {...{ el: labelBookCard(el.title), border: true }}>
+            <InfoBookAbout {...{ el, border: true, abs: true }} />
+          </DropStats>
+
+          <DropStats {...{ el: labelInfo, border: true }}>
+            <DropStats
+              {...{
+                el: labelDataBook,
+                abs: true,
+                fields: showGeneralStatsBook(el),
+              }}
+            />
+
+            <RatingFancy {...{ el, abs: true }} />
+          </DropStats>
+
+          <DropStats
+            {...{
+              el: libraryLabelStoreDynamic(el?.store?.name ?? ""),
+              border: true,
+            }}
+          >
+            <DropStats
+              {...{
+                el: labelFieldAddressStore,
+                abs: true,
+                fields: showStoreAddressFromBook(el),
+              }}
+            />
+
+            <DropStats
+              {...{
+                el: labelCategories,
+                abs: true,
+              }}
+            >
+              {el?.store?.categories?.map((el, i) => (
+                <li
+                  key={ids?.[0]?.[i] ?? i}
+                  className="w-full flex justify-start"
+                >
+                  <span className="txt__2">{el}</span>
+                </li>
+              ))}
+            </DropStats>
+
+            <DropStats
+              {...{
+                el: labelDelivery,
+                abs: true,
+                fields: statsDeliveryStoreFromBook(el),
+              }}
+            />
+          </DropStats>
         </div>
       </div>
 
