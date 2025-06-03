2524da497cdc6b39412e97387bcf82e3b1751ac3
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-03 08:23:28 +0300

    refactored query user

--- server/src/controllers/user/get.ts
+++ server/src/controllers/user/get.ts
@@ -5,9 +5,9 @@ import { res200, res204 } from "../../lib/responseClient/res.js";
 import { Thumb } from "../../models/all/img&video/Thumb.js";
 import { BookStore } from "../../models/all/BookStore.js";
 import { BookStoreUser } from "../../models/all/BookStoreUser.js";
-import { Op } from "sequelize";
+import { literal, Op } from "sequelize";
 import { Book } from "../../models/all/Book.js";
-import { err401 } from "../../lib/responseClient/err.js";
+import { err401, err404 } from "../../lib/responseClient/err.js";
 
 export const getUserProfile = async (
   req: ReqApp,
@@ -21,6 +21,67 @@ export const getUserProfile = async (
     // attributes is like select in mongoose where u specify what u want or what to exclude with - like (-password, ...ecc)
     attributes: {
       exclude: ["password", "createdAt", "updatedAt", "tempEmail"],
+      include: [
+        [
+          literal(`(
+      SELECT CASE 
+        WHEN COUNT(*) > 0 THEN true 
+        ELSE false 
+      END
+      FROM "book_stores" AS bs
+      WHERE bs."ownerID" = "User"."id"
+          )`),
+          "isOwner",
+        ],
+        [
+          literal(`(
+          SELECT CASE 
+            WHEN COUNT(*) > 0 THEN true 
+            ELSE false 
+          END
+          FROM "book_stores_users" AS bsu
+          WHERE bsu."userID" = "User"."id"
+          )`),
+          "isWorker",
+        ],
+        [
+          literal(`(
+          SELECT CASE 
+            WHEN COUNT(*) > 0 THEN true 
+            ELSE false 
+          END
+          FROM "book_stores" AS bs
+          INNER JOIN "books" AS b ON bs."id" = b."bookStoreID"
+          WHERE bs."ownerID" = "User"."id"
+          )`),
+          "hasBooks",
+        ],
+        [
+          literal(`(
+    SELECT CASE 
+      WHEN COUNT(*) > 0 THEN true 
+      ELSE false 
+    END
+    FROM "book_stores" AS bs
+    INNER JOIN "book_stores_users" AS bsu ON bs."id" = bsu."bookStoreID"
+    INNER JOIN "users" AS u ON bsu."userID" = u."id"
+    WHERE bs."ownerID" = "User"."id"
+            )`),
+          "hasWorkers",
+        ],
+        [
+          literal(`(
+          SELECT CASE 
+            WHEN COUNT(*) > 0 THEN true 
+            ELSE false 
+          END
+          FROM "carts" AS c
+          INNER JOIN "cart_items" AS ci ON c."id" = ci."cartID"
+          WHERE c."userID" = "User"."id"
+          )`),
+          "hasCart",
+        ],
+      ],
     },
     // include is like populate in mongoose ODM( object data modelling instead of object relational mapping) that allow u to get the data referenced by id after `populate` that field with real data and not just leaving the id as string
     include: [
@@ -30,50 +91,16 @@ export const getUserProfile = async (
         as: "thumb",
       },
     ],
+
     // raw is like lean or toObject in mongoose and it allow u to to get data as js obj and not as document mongoDB, in this case postGres model, it also make operation faster
     raw: true,
     // nest exist just in sql, in mongoDB objects are naturally nested by default and u do not need to specify it as characteristic
     nest: true,
   });
 
-  const bookStores = await BookStore.findAll({
-    where: {
-      ownerID: userID,
-    },
-  });
-  const isOwner = !!bookStores.length;
-  const isWorker = !!(await BookStoreUser.count({
-    where: {
-      userID,
-    },
-  }));
-  const teams = await BookStoreUser.findAll({
-    where: {
-      bookStoreID: {
-        [Op.in]: bookStores.map((bookStore) => bookStore.id),
-      },
-    },
-  });
-  const hasWorkers = !!teams.length;
-  const books = await Book.count({
-    where: {
-      bookStoreID: {
-        [Op.in]: bookStores.map((bookStore) => bookStore.id),
-      },
-    },
-  });
-  const hasBooks = !!books;
-
-  // const user = userInstance?.get({ plain: true });
-  // return err401(res, { msg: MsgErrSession.ACCESS_INVALID });
+  if (!user) return err404(res, { msg: "user not found" });
 
   return res200(res, {
-    user: {
-      ...user,
-      isOwner,
-      isWorker,
-      hasWorkers,
-      hasBooks,
-    },
+    user: user,
   });
 };
