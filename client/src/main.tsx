bb20a2947f0bdd74e9b674743b992bf5cf612002
alex <matveevalexander470@gmail.com>
2025-06-04 16:46:50 +0300

    _

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,8 +7,6 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import FormsCtxProvider from "./core/contexts/FormsCtx/FormsCtxProvider.tsx";
 
-// aa
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider {...{ store }}>
