174f0c768a280f4f70ac22a6d056a3416c267836
alex <matveevalexander470@gmail.com>
2025-06-04 16:38:05 +0300

    main branh

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import FormsCtxProvider from "./core/contexts/FormsCtx/FormsCtxProvider.tsx";
 
+// aa
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider {...{ store }}>
