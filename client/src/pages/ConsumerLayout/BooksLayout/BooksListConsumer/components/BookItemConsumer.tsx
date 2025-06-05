c78599956125bde12e5d61e9c3ffa9706559c47f
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-05 06:53:47 +0300

    working refacotor card item book conusmer

--- client/src/pages/ConsumerLayout/BooksLayout/BooksListConsumer/components/BookItemConsumer.tsx
+++ client/src/pages/ConsumerLayout/BooksLayout/BooksListConsumer/components/BookItemConsumer.tsx
@@ -1,45 +1,27 @@
 import ImagesItem from "@/components/elements/imagesHandlers/ImagesItem";
 import LinksCard from "@/components/elements/cards/shared/LinksCard";
-import {
-  linksBookConsumer,
-  showStoreAddressFromBook,
-  statsDeliveryStoreFromBook,
-} from "@/features/ConsumerLayout/BooksLayout/fields/card";
+import { linksBookConsumer } from "@/features/ConsumerLayout/BooksLayout/fields/card";
 import { BookType } from "@/types/all/books";
 import type { FC } from "react";
-import DropStats from "@/components/elements/dropMenus/dropSimple/DropStats";
-import {
-  labelBookCard,
-  labelDataBook,
-  showGeneralStatsBook,
-} from "@/core/config/fieldsData/books/cards";
-import InfoBookAbout from "@/components/elements/cards/books/subComponents/InfoBookAbout";
-import {
-  labelBookTitle,
-  labelCategories,
-  labelInfo,
-  libraryLabelStoreDynamic,
-} from "@/core/config/fieldsData/labels/shared";
-import RatingFancy from "@/components/elements/cards/shared/rating/RatingFancy";
-import {
-  labelDelivery,
-  labelFieldAddressStore,
-} from "@/core/config/fieldsData/bookStores/cards";
-import { useCreateIds } from "@/core/hooks/all/UI/useCreateIds";
+import { labelBookTitle } from "@/core/config/fieldsData/labels/shared";
 import SpanTitleCard from "@/components/elements/cards/shared/SpanTitleCard";
+import SpanInfoCard from "@/components/elements/cards/shared/SpanInfoCard";
+import { TbPigMoney } from "react-icons/tb";
+import { priceFormatter } from "@/core/lib/lib";
+import RatingItem from "@/components/elements/cards/shared/rating/RatingItem";
 
 type PropsType = {
   el: BookType;
 };
 
 const BookItemConsumer: FC<PropsType> = ({ el }) => {
-  const ids = useCreateIds({ lengths: [el?.store?.categories?.length] });
-
   return (
-    <div className="w-full grid grid-cols-1 gap-x-10 border-[3px] border-neutral-800 rounded-xl p-3 gap-y-5">
-      <div className="w-full grid grid-cols-1 h-fit items-start gap-y-5">
+    <div className="card_list">
+      <div className="w-full">
         <SpanTitleCard {...{ label: labelBookTitle(el.title) }} />
+      </div>
 
+      <div className="w-full grid grid-cols-1 h-fit items-start gap-y-5">
         <ImagesItem {...{ images: el.images }} />
       </div>
 
@@ -49,60 +31,15 @@ const BookItemConsumer: FC<PropsType> = ({ el }) => {
             el.images?.length ? "" : "-mt-5"
           }`}
         >
-          <DropStats {...{ el: labelBookCard(el.title), border: true }}>
-            <InfoBookAbout {...{ el, border: true, abs: true }} />
-          </DropStats>
+          <div className="w-full flex justify-start">
+            <RatingItem {...{ rat: el.ratingStats.avgRating }} />
+          </div>
 
-          <DropStats {...{ el: labelInfo, border: true }}>
-            <DropStats
-              {...{
-                el: labelDataBook,
-                abs: true,
-                fields: showGeneralStatsBook(el),
-              }}
-            />
-
-            <RatingFancy {...{ el, abs: true }} />
-          </DropStats>
-
-          <DropStats
+          <SpanInfoCard
             {...{
-              el: libraryLabelStoreDynamic(el?.store?.name ?? ""),
-              border: true,
+              spanInfo: { icon: TbPigMoney, info: priceFormatter(el.price) },
             }}
-          >
-            <DropStats
-              {...{
-                el: labelFieldAddressStore,
-                abs: true,
-                fields: showStoreAddressFromBook(el),
-              }}
-            />
-
-            <DropStats
-              {...{
-                el: labelCategories,
-                abs: true,
-              }}
-            >
-              {el?.store?.categories?.map((el, i) => (
-                <li
-                  key={ids?.[0]?.[i] ?? i}
-                  className="w-full flex justify-start"
-                >
-                  <span className="txt__2">{el}</span>
-                </li>
-              ))}
-            </DropStats>
-
-            <DropStats
-              {...{
-                el: labelDelivery,
-                abs: true,
-                fields: statsDeliveryStoreFromBook(el),
-              }}
-            />
-          </DropStats>
+          />
         </div>
 
         <LinksCard {...{ ID: el.id, links: linksBookConsumer }} />
