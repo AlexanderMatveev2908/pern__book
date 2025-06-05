cddd9cfe697892c789233171fcd29ca711e67ad1
alex <matveevalexander470@gmail.com>
2025-06-05 09:28:11 +0300

    start summary cart

--- client/src/pages/ConsumerLayout/CartLayout/CartPage/components/subComponents/WrapTxt.tsx
+++ client/src/pages/ConsumerLayout/CartLayout/CartPage/components/subComponents/WrapTxt.tsx
@@ -16,7 +16,7 @@ const WrapTxt: FC<PropsType> = ({ arg }) => {
         {arg[0]}
       </span>
       <span
-        className="txt__2 clamp_txt justify-self-end"
+        className="txt__2 clamp_txt justify-self-end text-end"
         style={{
           lineClamp: 2,
         }}
