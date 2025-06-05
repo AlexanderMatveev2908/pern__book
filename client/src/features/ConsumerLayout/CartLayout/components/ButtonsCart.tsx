4d2842ed0c987570cd90a6586c45d16c9c2a8096
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-05 19:37:58 +0300

    test_2

--- client/src/features/ConsumerLayout/CartLayout/components/ButtonsCart.tsx
+++ client/src/features/ConsumerLayout/CartLayout/components/ButtonsCart.tsx
@@ -64,7 +64,6 @@ const ButtonsCart: FC<PropsType> = ({ book, cart }) => {
             </div>
           </div>
           <div className="w-full grid grid-cols-2 justify-items-center items-center">
-            {/* aaa */}
             <WrapBtnCart
               {...{
                 label: labelDecQtyCart,
