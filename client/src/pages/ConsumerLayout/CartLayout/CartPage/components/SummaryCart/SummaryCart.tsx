ce97ad63467fbe7eac0057e4ee5a471b33f18388
alex <matveevalexander470@gmail.com>
2025-06-05 13:46:53 +0300

    need group books by store

--- client/src/pages/ConsumerLayout/CartLayout/CartPage/components/SummaryCart/SummaryCart.tsx
+++ client/src/pages/ConsumerLayout/CartLayout/CartPage/components/SummaryCart/SummaryCart.tsx
@@ -1,10 +1,10 @@
 import Title from "@/components/elements/Title";
 import { useGetCart } from "@/core/hooks/all/api/useGetCart";
 import { useCreateIds } from "@/core/hooks/all/UI/useCreateIds";
-import { isArrOk, priceFormatter } from "@/core/lib/lib";
+import { getDeliveryPrice, isArrOk, priceFormatter } from "@/core/lib/lib";
 import WrapTxt from "@/components/elements/WrapPairTxt/WrapTxt";
 import { X } from "lucide-react";
-import type { FC } from "react";
+import { type FC } from "react";
 
 const SummaryCart: FC = () => {
   const { cart } = useGetCart();
@@ -12,19 +12,19 @@ const SummaryCart: FC = () => {
   const ids = useCreateIds({ lengths: [cart?.items?.length] });
 
   return (
-    <div className="sticky top-[85px] p-4 pt-0 border-[3px] border-blue-600 rounded-xl grid grid-cols-1 gap-y-4 z-60 bg-neutral-950 w-full max-w-[800px] max-h-[300px] overflow-y-auto scroll_app scroll_y">
-      <div className="w-full sticky z-60 top-0 py-4 border-b-2 border-blue-600 bg-neutral-950">
+    <div className="sticky top-[85px] p-4 pt-0 border-[3px] border-blue-600 rounded-xl grid grid-cols-1 gap-y-4 z-60 bg-neutral-950 w-full max-w-[800px] ">
+      <div className="w-full sticky z-60 top-0 py-4 border-b-[3px] border-blue-600 bg-neutral-950">
         <Title {...{ title: "summary cart" }} />
       </div>
 
-      <div className="w-full grid grid-cols-1 gap-y-4">
+      <div className="w-full grid grid-cols-1 gap-y-4 max-h-[200px] overflow-y-auto scroll_app scroll_y">
         {isArrOk(cart?.items) &&
           cart!.items!.map((el, i) => (
             <div
               key={ids[0][i]}
-              className="w-full grid grid-cols-1 items-center gap-y-3"
+              className="w-full grid grid-cols-1 items-center gap-y-3 pl-3 border-l-2 border-blue-600"
             >
-              <div className="w-full flex justify-start max-w-full">
+              <div className="w-full flex justify-center max-w-full">
                 <span
                   className="txt__3 clamp_txt"
                   style={{
@@ -44,7 +44,17 @@ const SummaryCart: FC = () => {
                 <WrapTxt {...{ txt: priceFormatter(el.book!.price) }} />
               </div>
 
-              <div className="w-full flex justify-center"></div>
+              <div className="w-full grid grid-cols-3 items-center justify-items-center">
+                <WrapTxt {...{ txt: "delivery" }} />
+
+                <div className=""></div>
+
+                <WrapTxt
+                  {...{
+                    txt: getDeliveryPrice({ cart, store: el.book!.store! }),
+                  }}
+                />
+              </div>
             </div>
           ))}
       </div>
