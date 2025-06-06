9d57c9948489adc4d334f51f8481e0fc37600d6a
alex <matveevalexander470@gmail.com>
2025-06-06 13:29:26 +0300

    update also card worker

--- client/src/pages/OwnerLayout/BookStoreLayout/BookStores/components/BookStoreItem.tsx
+++ client/src/pages/OwnerLayout/BookStoreLayout/BookStores/components/BookStoreItem.tsx
@@ -1,53 +1,18 @@
 import ItemID from "@/components/elements/cards/shared/ItemID";
 import { linksCardStore } from "@/features/OwnerLayout/bookStores/fields/card";
 import { BookStoreType } from "@/types/all/bookStore";
-import { FC, useMemo } from "react";
-import ImagesItem from "@/components/elements/imagesHandlers/ImagesItem/ImagesItem";
-import SpanInfoCard from "@/components/elements/cards/shared/SpanInfoCard";
-import { HiLibrary } from "react-icons/hi";
-import { FaCity } from "react-icons/fa";
-import PairBtnsLink from "@/components/elements/cards/shared/PairBtnsLink";
-import { isArrOk } from "@/core/lib/lib";
+import { FC } from "react";
+import StoreItemList from "@/components/elements/cards/bookstore/StoreItemList";
 
 type PropsType = {
   el: BookStoreType;
 };
 
 const BookStoreItemOwner: FC<PropsType> = ({ el }) => {
-  const hasImages = useMemo(() => isArrOk(el.images), [el]);
-
   return (
     <div className="item">
       <ItemID {...{ ID: el.id }} />
-
-      <div className="content">
-        {hasImages && (
-          <div className="images_wrapper">
-            <ImagesItem {...{ images: el.images }} />
-          </div>
-        )}
-
-        <div className={`info_wrapper ${!hasImages ? "col-span-2" : ""}`}>
-          <SpanInfoCard
-            {...{
-              spanInfo: {
-                label: el.name,
-                icon: HiLibrary,
-              },
-            }}
-          />
-          <SpanInfoCard
-            {...{
-              spanInfo: {
-                label: el.city,
-                icon: FaCity,
-              },
-            }}
-          />
-
-          <PairBtnsLink {...{ links: linksCardStore(el.id) }} />
-        </div>
-      </div>
+      <StoreItemList {...{ el, links: linksCardStore(el.id) }} />
     </div>
   );
 };
--- client/src/pages/WorkerLayout/BookStoreLayout/BookStoreList/BookStoreListWorker.tsx
+++ client/src/pages/WorkerLayout/BookStoreLayout/BookStoreList/BookStoreListWorker.tsx
@@ -66,7 +66,7 @@ const BookStoreListWorker: FC = () => {
         </FormProvider>
 
         <WrapperContentAPI {...({ formCtx, hook } as any)}>
-          <div className="p_cards">
+          <div className="list_items">
             {isArr(bookStores) &&
               bookStores!.map((el) => (
                 <BookStoreItemWorker key={el.id} {...{ el }} />
