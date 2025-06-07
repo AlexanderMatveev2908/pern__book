582096f9a5432b7d8ebcfd501f5b68c61077f8d3
alex <matveevalexander470@gmail.com>
2025-06-07 15:52:14 +0300

    _

--- tests/src/db/types/all/userTypes.ts
+++
@@ -1,6 +0,0 @@
-export enum UserRole {
-  CUSTOMER = "CUSTOMER",
-  EMPLOYEE = "EMPLOYEE",
-  MANAGER = "MANAGER",
-  OWNER = "OWNER",
-}
--- tests/src/db/types/types.ts
+++
@@ -1,3 +0,0 @@
-export * from "./all/general.js";
-export * from "./all/tokenTypes.js";
-export * from "./all/userTypes.js";
