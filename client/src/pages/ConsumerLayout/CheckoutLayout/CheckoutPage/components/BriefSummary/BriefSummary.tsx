2ad84ee58a94e49a462fe8cadb7f0e9cb67e1579
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-08 10:14:29 +0300

    added hardcoded discount line checkout page

--- client/src/pages/ConsumerLayout/CheckoutLayout/CheckoutPage/components/BriefSummary/BriefSummary.tsx
+++ client/src/pages/ConsumerLayout/CheckoutLayout/CheckoutPage/components/BriefSummary/BriefSummary.tsx
@@ -29,19 +29,37 @@ const BriefSummary: FC<PropsType> = ({ groupedByStoreID }) => {
       <Title {...{ title: "summary", styleTxt: "txt__4" }} />
 
       <div className="w-full max-w-[500px] sm:max-w-[600px] border-[3px] border-neutral-800 rounded-xl gap-y-6 grid grid-cols-1 ">
-        <div className="w-full sticky top-0 bg-neutral-950 z-60 grid grid-cols-1 pb-2 border-b-2 border-blue-600 py-2">
+        <div className="w-full sticky top-0 bg-neutral-950 z-60 grid grid-cols-1 pb-2 border-b-2 border-blue-600 py-2 gap-y-3 rounded-t-xl">
+          <WrapPairTxt
+            {...{
+              arg: ["subtotal", priceFormatter(totalCart)],
+              customStyles: [
+                "justify-self-center txt__2",
+                "justify-self-center txt__2",
+              ],
+            }}
+          />
+          <WrapPairTxt
+            {...{
+              arg: ["discount", priceFormatter(0)],
+              customStyles: [
+                "justify-self-center txt__2",
+                "justify-self-center txt__2",
+              ],
+            }}
+          />
           <WrapPairTxt
             {...{
               arg: ["Total", priceFormatter(totalCart)],
               customStyles: [
-                "justify-self-center txt__4",
-                "justify-self-center txt__4",
+                "justify-self-center txt__3",
+                "justify-self-center txt__3",
               ],
             }}
           />
         </div>
 
-        <div className="max-h-[325px] overflow-y-auto scroll_app scroll_y flex flex-col gap-y-6  pb-10 px-4">
+        <div className="max-h-[250px] overflow-y-auto scroll_app scroll_y flex flex-col gap-y-6  pb-10 px-4">
           {Object.values(groupedByStoreID).map(({ store, items }, outerIdx) => (
             <SummaryStoreItem key={ids[0][outerIdx]} {...{ store, items }} />
           ))}
--- server/src/routes/routes/consumerRoutes/consumer.ts
+++ server/src/routes/routes/consumerRoutes/consumer.ts
@@ -2,9 +2,16 @@ import express from "express";
 import consumerBooksRouter from "./books/books.js";
 import cartRouter from "./cart/cartRoutes.js";
 import { verifyAccessToken } from "../../../middleware/protected/verifyAccessToken.js";
+import checkoutRouter from "./checkout/checkoutRoutes.js";
 
 const consumerRouter = express.Router();
 
 consumerRouter.use("/books", consumerBooksRouter);
 consumerRouter.use("/cart", verifyAccessToken({}), cartRouter);
+consumerBooksRouter.use(
+  "/checkout",
+  verifyAccessToken({ isVerified: true }),
+  checkoutRouter
+);
+
 export default consumerRouter;
