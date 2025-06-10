5e7812e971a79d049aeb5ac6e9bd348e569589b5
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-10 13:52:35 +0300

    _

--- client/src/features/OwnerLayout/orders/ownerOrdersSliceAPI.ts
+++ client/src/features/OwnerLayout/orders/ownerOrdersSliceAPI.ts
@@ -1,8 +1,8 @@
-import apiSlice from "@/store/apiSlice";
-import { BaseResAPI } from "@/types/types";
+// import apiSlice from "@/store/apiSlice";
+// import { BaseResAPI } from "@/types/types";
 
-export const ownerOrdersSliceAPI = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    getOwnerOrdersList: builder.query<BaseResAPI<void>, void>({}),
-  }),
-});
+// export const ownerOrdersSliceAPI = apiSlice.injectEndpoints({
+//   endpoints: (builder) => ({
+//     getOwnerOrdersList: builder.query<BaseResAPI<void>, void>(),
+//   }),
+// });
