39d3dc698579022e89287b00b9b89adfd5faaa43
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-13 06:54:36 +0300

    refactoring architecture towards more feature based mindset

--- client/src/app/routes/all/AuthRoutes.tsx
+++ client/src/app/routes/all/AuthRoutes.tsx
@@ -2,7 +2,7 @@ import SwapCtxProvider from "@/core/contexts/SwapCtx/SwapAddressProvider";
 import AuthLayout from "@/app/layouts/AuthLayout";
 import ChoseNewPwd from "@/pages/AuthLayout/ChoseNewPwd/ChoseNewPwd";
 import ForgotPwd from "@/pages/AuthLayout/ForgotPwd/ForgotPwd";
-import Login from "@/pages/AuthLayout/Login/Login";
+import LoginPage from "@/pages/AuthLayout/LoginPage";
 import RegisterPage from "@/pages/AuthLayout/RegisterPage";
 import VerifyAccount from "@/pages/AuthLayout/VerifyAccount/VerifyAccount";
 import Push from "../helpers/Push";
@@ -19,7 +19,7 @@ export const authRoutes = {
         </SwapCtxProvider>
       ),
     },
-    { path: "login", element: <Login /> },
+    { path: "login", element: <LoginPage /> },
     { path: "verify-account", element: <VerifyAccount /> },
     { path: "forgot-pwd", element: <ForgotPwd /> },
     { path: "chose-new-pwd", element: <ChoseNewPwd /> },
