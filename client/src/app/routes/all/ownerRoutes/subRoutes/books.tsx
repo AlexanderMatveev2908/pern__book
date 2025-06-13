f5194405fbd9f10a332e03646be95740e9686f1f
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-13 08:12:04 +0300

    refactoring architecture towards more feature based mindset

--- client/src/app/routes/all/ownerRoutes/subRoutes/books.tsx
+++ client/src/app/routes/all/ownerRoutes/subRoutes/books.tsx
@@ -1,7 +1,7 @@
 import SearchCtxProvider from "@/core/contexts/SearchCtx/SearchCtxProvider";
 import BooksList from "@/pages/OwnerLayout/BooksLayout/BooksList/BooksList";
 import CreateBook from "@/pages/OwnerLayout/BooksLayout/CreateBook/CreateBook";
-import UpdateBook from "@/pages/OwnerLayout/BooksLayout/UpdateBook/UpdateBook";
+import UpdateBookPageOwner from "@/pages/OwnerLayout/BooksLayout/UpdateBookPageOwner";
 import { Outlet } from "react-router-dom";
 import BookPageOwner from "@/pages/OwnerLayout/BooksLayout/BookPageOwner";
 import Push from "@/app/routes/helpers/Push";
@@ -11,7 +11,7 @@ export const booksRoutes = {
   element: <Outlet />,
   children: [
     { path: "add-book", element: <CreateBook /> },
-    { path: "update/:bookID", element: <UpdateBook /> },
+    { path: "update/:bookID", element: <UpdateBookPageOwner /> },
     {
       path: "list",
       element: (
