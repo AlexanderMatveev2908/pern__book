bca294ac6d2d64fbed3d27b1a10abb0861ead224
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-15 16:02:21 +0300

    changed populate fn

--- server/src/stuff/populateDB/populateDB.ts
+++ server/src/stuff/populateDB/populateDB.ts
@@ -2,13 +2,14 @@ import { seq } from "../../config/db.js";
 import { hashPwd } from "../../lib/hashEncryptSign/argon.js";
 import { __cg } from "../../lib/utils/log.js";
 import { BookStore } from "../../models/all/BookStore.js";
-import { BookStoreUser } from "../../models/all/BookStoreUser.js";
 import { User } from "../../models/all/User.js";
-import { UserRole } from "../../types/types.js";
 import { doLorem, makeRandomMinMax, pickRandom } from "./placeHolders.js";
-import { catStores, fillDefVals, getValidCat, users } from "./data.js";
-import { Book } from "../../models/all/Book.js";
-import { createBooksAssets } from "./helpers.js";
+import { catStores, users } from "./data.js";
+import {
+  createBooksAssets,
+  generateBooks,
+  generateJunction,
+} from "./helpers.js";
 
 export const populateDB = async () => {
   const safeUsers = await Promise.all(
@@ -53,94 +54,74 @@ export const populateDB = async () => {
     const tyler = newUsers.find((w) => w.email === "tyler@gmail.com");
 
     for (const u of newUsers) {
+      const store = await BookStore.findOne({
+        where: {
+          ownerID: u.id,
+        },
+        transaction: t,
+      });
+
       switch (u.email) {
         case "jane@gmail.com": {
-          const store = await BookStore.findOne({
-            where: {
-              ownerID: u.id,
-            },
-            transaction: t,
+          await generateJunction({
+            email: "jane@gmail.com",
+            tyler: tyler!,
+            store: store!,
+            t,
           });
-
-          await BookStoreUser.create(
-            {
-              userID: tyler!.id,
-              bookStoreID: store!.id,
-              userEmail: tyler!.email,
-              role: UserRole.MANAGER,
-            },
-            { transaction: t }
-          );
 
           const { uploaded, images } = await createBooksAssets([
             "virginia_woolf",
           ]);
 
-          let i = 0;
+          await generateBooks({
+            uploaded,
+            images,
+            t,
+            u,
+            store: store!,
+          });
 
-          while (i < uploaded.length) {
-            await Book.create(
-              {
-                title: images[i].title,
-                author: "virginia_woolf",
-                ...fillDefVals({ min: 1900, max: 1942, u, store: store! }),
-                images: [
-                  {
-                    url: uploaded[i].url,
-                    publicID: uploaded[i].publicID,
-                  },
-                ],
-              },
-              { transaction: t }
-            );
-            i++;
-          }
+          break;
+        }
+
+        case "tyler@gmail.com": {
+          const { uploaded, images } = await createBooksAssets([
+            "albert_camus",
+            "jean_paul_sartre",
+            "jean_jack_rousseau",
+          ]);
+
+          await generateBooks({
+            uploaded,
+            images,
+            t,
+            u,
+            store: store!,
+          });
 
           break;
         }
 
         case "john@gmail.com": {
-          const store = await BookStore.findOne({
-            where: {
-              ownerID: u.id,
-            },
-            transaction: t,
+          await generateJunction({
+            email: "john@gmail.com",
+            tyler: tyler!,
+            store: store!,
+            t,
           });
-
-          await BookStoreUser.create(
-            {
-              userID: tyler!.id,
-              userEmail: tyler!.email,
-              bookStoreID: store!.id,
-              role: UserRole.EMPLOYEE,
-            },
-            { transaction: t }
-          );
 
           const { uploaded, images } = await createBooksAssets([
             "hernest_hemingway",
           ]);
 
-          let i = 0;
-
-          while (i < uploaded.length) {
-            await Book.create(
-              {
-                title: images[i].title,
-                author: "hernest_hemingway",
-                ...fillDefVals({ min: 1920, max: 1961, u, store: store! }),
-                images: [
-                  {
-                    url: uploaded[i].url,
-                    publicID: uploaded[i].publicID,
-                  },
-                ],
-              },
-              { transaction: t }
-            );
-
-            i++;
-          }
+          await generateBooks({
+            uploaded,
+            images,
+            t,
+            u,
+            store: store!,
+          });
 
           break;
         }
