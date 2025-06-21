825a9085b9d940eab3eb3ec834eac277b9ee993b
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-21 17:00:20 +0300

    _

--- tests/src/e2e/features/owner/openStore.test.ts
+++ tests/src/e2e/features/owner/openStore.test.ts
@@ -1,6 +1,11 @@
 import { expect, test } from "@playwright/test";
 import { handleLoginT } from "../../lib/login.js";
-import { clickBtn, clickLink, nav, searchTxt } from "../../lib/general.js";
+import {
+  clickBtn,
+  clickLink,
+  searchTxt,
+  waitRedirect,
+} from "../../lib/general.js";
 
 test.beforeEach(async ({ page }) => {
   await handleLoginT({ page });
@@ -9,11 +14,16 @@ test.beforeEach(async ({ page }) => {
 test.describe("", () => {
   test("✅", async ({ page }) => {
     await clickBtn({ page, aria: "toggle sidebar" });
-    await searchTxt({ page, txt: "Admin area", timeout: 10000 });
-    await searchTxt({ page, txt: "My Account", timeout: 10000 });
+    await searchTxt({ page, txt: "Admin area" });
+    await searchTxt({ page, txt: "My Account" });
 
-    await clickBtn({ page, aria: "Admin area dropdown", timeout: 10000 });
-    await searchTxt({ page, txt: "Open a bookstore", timeout: 10000 });
+    await clickBtn({ page, aria: "Admin area dropdown" });
+    await searchTxt({ page, txt: "Open a bookstore" });
+
+    await clickLink({ page, aria: "Open a bookstore sidebar" });
+    await waitRedirect(page, `owner/book-store/create`);
+
+    await searchTxt({ page, txt: "🟢 Fields marked in green are required" });
   });
 
   //   test("🚫", async ({ page }) => {});
